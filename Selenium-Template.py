554b03f68ee79a0905b91afd2393a273c96b0f6c
antonydp <38143733+antonydp@users.noreply.github.com>
2022-11-06 12:19:44 +0100

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -33,8 +33,9 @@ for option in options:
     
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get('http://github.com')
+driver.get('https://www.tantifilm.rodeo')
 print(driver.title)
+print(driver.url)
 with open('./GitHub_Action_Results.txt', 'w') as f:
     f.write(f"This was written with a GitHub action {driver.title}")
 
